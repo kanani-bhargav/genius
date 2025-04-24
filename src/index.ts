a651d0b6eed8cd29df156c37a707c157bb7b355c
bhargav <kananibhargavkd11@gmail.com>
2025-04-24 23:55:14 +0530

    test push

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import helloRouter from "./routes/hello";
 import byeRouter from "./routes/bye";
+import successRouter from "./routes/success";
 
 const app = express();
 const PORT = 3000;
 
 app.use("/api", helloRouter);
 app.use("/api", byeRouter);
+app.use("/api", successRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
